1193fe86b675a759170f023449921f249e2f5b3f
shisan <shisan@Xu.cn>
2023-10-16 16:22:00 +0800

    init

---
+++ configclient/src/main/java/com/shisan/ConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.shisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author:shisan
+ * @Date:2023/10/16 15:57
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ configserver/src/main/java/com/shisan/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.shisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author:shisan
+ * @Date:2023/10/16 15:37
+ */
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
